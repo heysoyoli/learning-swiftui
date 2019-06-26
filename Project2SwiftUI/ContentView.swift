1a3c967fa629edd38cbda05fd21b5d6f54a84151
Oliver Hernández <oliver@revolt.vc>
2019-06-25 22:30:55 -0500

    Initial Commit

---
+++ Project2SwiftUI/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Project2SwiftUI
+//
+//  Created by Oliver Hernández on 6/25/19.
+//  Copyright © 2019 heysoyoli. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
